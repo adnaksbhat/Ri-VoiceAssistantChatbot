f7a6855c44763cb4efe41b4864263a8aaab70825
Skanda Bhat <56406329+adnaksbhat@users.noreply.github.com>
2021-08-15 20:06:17 +0530

    String file

---
+++ string_calls.py
@@ -0,0 +1,50 @@
+#String Calls
+
+import pyttsx3
+import sys
+import time
+from playsound import playsound
+
+
+
+
+
+
+sssq=input("Welcome! Enter your name\n")
+
+qq22="Hello "+sssq+"! "+"\nI am your voice assistant chatbot 'Reee'\nHow can I help you,?"
+
+
+def bigString():
+
+   
+
+
+
+   print("_____________________________________\n")
+   print("Ready to provide assistance\nAsk to create ToDo List\nAsk to show ToDo List\nAsk to create new file and add contents\n")
+   print("_____________________________________\n")
+
+   time.sleep(4)
+   qwq=sssq
+
+   blah="Loading. . . . . . . . . . .\n\n"
+
+   for l in blah:
+      sys.stdout.write(l)
+      sys.stdout.flush()
+      time.sleep(0.2)
+
+   qq="Hello "+qwq+"! "+"\nI am your voice assistant chatbot 'Ri'"
+   print(qq)
+  
+
+
+
+
+
+
+
+
+
+
